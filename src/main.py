3034ec6603d15a9e355adf4085fad701834ec2e5
KevinSci <zckevin9@gmail.com>
2026-03-09 13:37:54 -0600

    Instalar resemblyzer

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, File, UploadFile, HTTPException
 from sqlalchemy.orm import Session
 from .routers import users
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,3 +27,17 @@ app.include_router(users.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+
+from resemblyzer import VoiceEncoder, preprocess_wav
+
+@app.post("/audio")
+async def proccess_audio(audio: UploadFile = File(...)):
+    if audio.content_type not in ["audio/mpeg", "audio/wav", "audio/mp3"]:
+        raise HTTPException(status_code=400, detail="Invalid file type")
+    wav_audio = preprocess_wav(audio.file)
+    encoder = VoiceEncoder()
+    embed = encoder.embed_utterance(wav_audio)
+    return embed
+
